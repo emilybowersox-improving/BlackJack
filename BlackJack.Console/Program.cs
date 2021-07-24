197cd3d5ab77004173cbc01d19a384d21e9f34bd
Emily Bowersox <emily.bowersox@improving.com>
2021-07-24 16:23:17 -0500

    need to add foreach loops to calclate and console write updated players hand when they choose 'hit'

--- BlackJack.Console/Program.cs
+++ BlackJack.Console/Program.cs
@@ -31,16 +31,31 @@ namespace ConsoleApp
                 /*       foreach (var card in fullDeck)
                            Console.WriteLine($"{card.Number} {card.Suit}");*/
 
-                var dealerCard1 = fullDeck[0];
+                List<Card> dealerHand = new List<Card>()
+                {
+                    new Card() {Number = fullDeck[0].Number, Suit = fullDeck[0].Suit },
+                    new Card() {Number = fullDeck[1].Number, Suit = fullDeck[1].Suit }
+                };
+
+                List<Card> playerHand = new List<Card>()
+                {
+                    new Card() {Number = fullDeck[2].Number, Suit = fullDeck[2].Suit },
+                    new Card() {Number = fullDeck[3].Number, Suit = fullDeck[3].Suit }
+                };
+
+                //// working on converting from variables to Lists for dealer and player hands 
+
+
+ /*               var dealerCard1 = fullDeck[0];
                 var dealerCard2 = fullDeck[1];
                 var playerHand1 = fullDeck[2];
-                var playerHand2 = fullDeck[3];
+                var playerHand2 = fullDeck[3];*/
 
                 Console.WriteLine("Dealer:");
-                Console.WriteLine($"{dealerCard1.Number} {dealerCard1.Suit}, {dealerCard2.Number} {dealerCard2.Suit}");
+                Console.WriteLine($"{dealerHand[0].Number} {dealerHand[0].Suit}, {dealerHand[1].Number} {dealerHand[1].Suit}");
 
                 Console.WriteLine("Player:");
-                Console.WriteLine($"{playerHand1.Number} {playerHand1.Suit}, {playerHand2.Number} {playerHand2.Suit}");
+                Console.WriteLine($"{playerHand[0].Number} {playerHand[0].Suit}, {playerHand[1].Number} {playerHand[1].Suit}");
 
                 fullDeck.RemoveRange(0, 4);
                 Console.WriteLine(fullDeck.Count());
@@ -54,8 +69,9 @@ namespace ConsoleApp
                     userResponse = Console.ReadLine();
                     if (userResponse == "hit")
                     {
-                        var playerCard3 = fullDeck[0];
-                        Console.WriteLine($"Your hand is now {playerCard3.Number} {playerCard3.Suit}, {playerHand1.Number} {playerHand1.Suit}, {playerHand2.Number} {playerHand2.Suit}");
+                        var newPlayerCard = fullDeck[0];
+                        playerHand.Add(newPlayerCard);
+                        Console.WriteLine($"Your hand is now {newPlayerCard.Number} {newPlayerCard.Suit}, {playerHand[0].Number} {playerHand[0].Suit}, {playerHand[1].Number} {playerHand[1].Suit}");
                     }
                     else if (userResponse == "stay")
                     {
@@ -68,9 +84,9 @@ namespace ConsoleApp
                 } while (userResponse != "stay");
 
 
-                int dealerHandCount = dealerCard1.Number + dealerCard2.Number;
+                int dealerHandCount = dealerHand[0].Number + dealerHand[1].Number;
                 Console.WriteLine($"Dealer hand count: {dealerHandCount}");
-                int playerHandCount = playerHand1.Number + playerHand2.Number;
+                int playerHandCount = playerHand[0].Number + playerHand[1].Number;
                 Console.WriteLine($"Player hand count: {playerHandCount}");
 
                 if (playerHandCount <= 21 && dealerHandCount > 21)
