328fc69eb074a12730ae447ea0fe1e514f990e38
Emily Bowersox <emily.bowersox@improving.com>
2021-07-24 16:02:13 -0500

    dealer and player hands need to be lists

--- BlackJack.Console/Program.cs
+++ BlackJack.Console/Program.cs
@@ -11,59 +11,21 @@ namespace ConsoleApp
         {
 
             var gamePlay = new GamePlay();
-     /*       Stack<Card> fullDeck = new Stack<Card>();
-            for (int i = 1; i <= 13; i++)
-            {
-                fullDeck.Push(new Card() { Suit = "Heart", Number = i });
-                fullDeck.Push(new Card() { Suit = "Diamond", Number = i });
-                fullDeck.Push(new Card() { Suit = "Club", Number = i });
-                fullDeck.Push(new Card() { Suit = "Spade", Number = i });
-            }
 
-            foreach (var card in fullDeck)
-                Console.WriteLine($"{card.Number} {card.Suit}");*/
-
-
-            List<Card> fullDeck = new List<Card>();
-            for (int i = 1; i <= 13; i++)
-            {
-                fullDeck.Add(new Card() { Suit = "Heart", Number = i });
-                fullDeck.Add(new Card() { Suit = "Diamond", Number = i });
-                fullDeck.Add(new Card() { Suit = "Club", Number = i });
-                fullDeck.Add(new Card() { Suit = "Spade", Number = i });
-            }
-    /*        foreach (var card in fullDeck)
-                Console.WriteLine($"{card.Number} {card.Suit}");*/
-
- /*           fullDeck = fullDeck.OrderBy(c => Guid.NewGuid()).ToList();
-
-            *//*       foreach (var card in fullDeck)
-                       Console.WriteLine($"{card.Number} {card.Suit}");*//*
-
-            var dealerCard1 = fullDeck[0];
-            var dealerCard2 = fullDeck[1];
-            var playerHand1 = fullDeck[2];
-            var playerHand2 = fullDeck[3];
-
-            Console.WriteLine("Dealer:");
-            Console.WriteLine($"{dealerCard1.Number} {dealerCard1.Suit}, {dealerCard2.Number} {dealerCard2.Suit}");
-
-            Console.WriteLine("Player:");
-            Console.WriteLine($"{playerHand1.Number} {playerHand1.Suit}, {playerHand2.Number} {playerHand2.Suit}"); 
-
-            fullDeck.RemoveRange(0, 4);
-            *//*      Console.WriteLine(fullDeck.Count());*//*
-
-            Console.WriteLine("___________________");
-            Console.WriteLine();
-
-            *//*          Console.WriteLine("Would you like to stay or hit?");
-                      var userReponse = Console.ReadLine();*/
             string userResponse = "";
             string userWantsToContinue = "";
 
             do
             {
+                List<Card> fullDeck = new List<Card>();
+                for (int i = 1; i <= 13; i++)
+                {
+                    fullDeck.Add(new Card() { Suit = "Heart", Number = i });
+                    fullDeck.Add(new Card() { Suit = "Diamond", Number = i });
+                    fullDeck.Add(new Card() { Suit = "Club", Number = i });
+                    fullDeck.Add(new Card() { Suit = "Spade", Number = i });
+                }
+
                 fullDeck = fullDeck.OrderBy(c => Guid.NewGuid()).ToList();
 
                 /*       foreach (var card in fullDeck)
@@ -81,7 +43,7 @@ namespace ConsoleApp
                 Console.WriteLine($"{playerHand1.Number} {playerHand1.Suit}, {playerHand2.Number} {playerHand2.Suit}");
 
                 fullDeck.RemoveRange(0, 4);
-                /*      Console.WriteLine(fullDeck.Count());*/
+                Console.WriteLine(fullDeck.Count());
 
                 Console.WriteLine("___________________");
                 Console.WriteLine();
